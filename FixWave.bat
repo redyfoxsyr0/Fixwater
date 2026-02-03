098ca8ae5097a30fa090ec0ca330f3bc8c7dfe3a
redyfoxsyr0 <redyfoxsyr0@hotmail.com>
2026-02-03 16:49:21 -0500

    Update FixWave.bat

--- FixWave.bat
+++ FixWave.bat
@@ -7,7 +7,7 @@ setlocal EnableExtensions EnableDelayedExpansion
 ::  of this script, in whole or in part, is strictly prohibited.
 set "CURRENT_VER=2.3.6"
 set "RAW_VER=https://raw.githubusercontent.com/redyfoxsyr0/Fixwater/refs/heads/main/version.txt"
-set "RAW_BAT=https://raw.githubusercontent.com/redyfoxsyr0/Fixwater/refs/heads/main/version.txt"
+set "RAW_BAT=https://raw.githubusercontent.com/redyfoxsyr0/Fixwater/refs/heads/main/FixWave.bat"
 for /f "delims=" %%D in ('powershell -NoProfile -Command "[Environment]::GetFolderPath('Desktop')"') do set "DESKTOP=%%D"
 set "NEWFILE=%DESKTOP%\FixWave.bat"
 set "LATEST_VER="
